a856a1668151710380328ce27dc5a61a5138b170
dirkli2011 <8350320@qq.com>
2019-01-23 10:43:57 +0800

    -

--- thirdparty.go
+++ thirdparty.go
@@ -4,6 +4,12 @@ import (
 	"github.com/cornelk/hashmap"
 )
 
+// 返回一个hashmap
 func NewHashMap() *hashmap.HashMap {
 	return &hashmap.HashMap{}
+}
+
+// 返回一个双向链表
+func NewList() *hashmap.List {
+	return hashmap.NewList()
 }
