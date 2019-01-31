6eb62b0e83956ad2935deea947a36d3ec9a716ff
dirkli2011 <8350320@qq.com>
2019-01-31 15:55:48 +0800

    -

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,20 +2,92 @@ package cache
 
 import (
 	"testing"
+	"time"
 )
 
-func TestMemcache(t *testing.T) {
-
-	cache, err := NewCache("memcache", `{"conn": "127.0.0.1:11211"}`)
+func TestCache(t *testing.T) {
+	memory, err := NewCache("memory", `{"interval": 1}`)
 	if err != nil {
 		t.Error("init err")
 	}
 
-	if err = cache.Set("dirkli", "1", 10); err != nil {
+	key := "dirkli"
+	val := "1111"
+	if err = memory.Set(key, val, 2); err != nil {
 		t.Error("set error", err)
 	}
 
-	if _, err := cache.Get("dirkli"); err != nil {
+	v, err := memory.Get("dirkli")
+	if err != nil {
 		t.Error("get error", err)
 	}
+	if v != val {
+		t.Error("get val fail")
+	}
+
+	if !memory.Exist(key) {
+		t.Error("exist, but return fail")
+	}
+
+	err = memory.Delete(key)
+	if err != nil {
+		t.Error("delete error", err)
+	}
+
+	if memory.Exist(key) {
+		t.Error("not exist, but return true")
+	}
+
+	num, err := memory.Incr(key)
+	if err != nil {
+		t.Error("Incr error", err)
+	}
+
+	if num != 1 {
+		t.Error("Incr fail")
+	}
+
+	for i := 0; i < 10; i++ {
+		num, err = memory.Incr(key)
+	}
+	if num != 11 {
+		t.Error("Incr fail")
+	}
+
+	for i := 0; i < 5; i++ {
+		num, err = memory.Decr(key)
+	}
+	if num != 6 {
+		t.Error("Decr fail")
+	}
+
+	valConf := map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+
+	for k, v := range valConf {
+		memory.Set(k, v, 3)
+	}
+
+	vals, err := memory.GetMulti([]string{"k1", "k2"})
+	if err != nil {
+		t.Error("GetMulti error", err)
+	}
+
+	for k, v := range valConf {
+		if v != vals[k] {
+			t.Error("GetMulti fail")
+		}
+	}
+	time.Sleep(4 * time.Second)
+	if memory.Exist("k1") || memory.Exist("k2") {
+		t.Error("interval delete expired key fail")
+	}
+
+	memory.ClearAll()
+	if memory.Exist(key) {
+		t.Error("expect not exist, but return true")
+	}
+
 }
---
+++ cache/memory.go
@@ -0,0 +1,183 @@
+package cache
+
+import (
+	"encoding/json"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var DefaultInterval = 60 //每分钟清理一次过期缓存
+
+type MemoryCacheItem struct {
+	val     string
+	ctime   time.Time
+	expired time.Duration
+}
+
+func (self *MemoryCacheItem) isExpired() bool {
+	if self.expired == 0 {
+		return false // 永不过期
+	}
+	return time.Now().Sub(self.ctime) > self.expired
+}
+
+type MemoryCache struct {
+	sync.RWMutex
+	interval time.Duration
+	items    map[string]*MemoryCacheItem
+}
+
+func init() {
+	Register("memory", Newcache)
+}
+
+func Newcache() Cache {
+	return &MemoryCache{items: make(map[string]*MemoryCacheItem)}
+}
+
+func (self *MemoryCache) Start(config string) error {
+	var cf map[string]int
+	json.Unmarshal([]byte(config), &cf)
+	if _, ok := cf["interval"]; !ok {
+		cf = make(map[string]int)
+		cf["interval"] = DefaultInterval
+	}
+
+	self.interval = time.Duration(cf["interval"]) * time.Second
+	go self.check()
+	return nil
+}
+
+func (self *MemoryCache) check() {
+
+	if self.interval < 1 {
+		return
+	}
+
+	for {
+		<-time.After(self.interval)
+		if self.items == nil {
+			continue
+		}
+
+		for key, item := range self.items {
+			if item.isExpired() {
+				self.Lock()
+				delete(self.items, key)
+				self.Unlock()
+			}
+		}
+	}
+}
+
+func (self *MemoryCache) Get(key string) (string, error) {
+	self.RLock()
+	defer self.RUnlock()
+	if v, ok := self.items[key]; ok {
+		if !v.isExpired() {
+			return v.val, nil
+		}
+	}
+	return "", nil
+}
+
+func (self *MemoryCache) Set(key string, val string, timeout int32) error {
+	self.Lock()
+	defer self.Unlock()
+	self.items[key] = &MemoryCacheItem{
+		val:     val,
+		ctime:   time.Now(),
+		expired: time.Duration(timeout) * time.Second,
+	}
+	return nil
+}
+
+func (self *MemoryCache) Delete(key string) error {
+	self.Lock()
+	defer self.Unlock()
+	if _, ok := self.items[key]; ok {
+		delete(self.items, key)
+	}
+	return nil
+}
+
+func (self *MemoryCache) Incr(key string) (uint64, error) {
+	self.Lock()
+	defer self.Unlock()
+
+	v, ok := self.items[key]
+	if !ok {
+		self.items[key] = &MemoryCacheItem{
+			val:     "1",
+			ctime:   time.Now(),
+			expired: 0,
+		}
+		return uint64(1), nil
+	}
+
+	val, _ := strconv.Atoi(v.val)
+	val++
+	self.items[key] = &MemoryCacheItem{
+		val:     strconv.Itoa(val),
+		ctime:   time.Now(),
+		expired: 0,
+	}
+	return uint64(val), nil
+}
+
+func (self *MemoryCache) Decr(key string) (uint64, error) {
+	self.RLock()
+	defer self.RUnlock()
+
+	v, ok := self.items[key]
+	if !ok {
+		self.items[key] = &MemoryCacheItem{
+			val:     "0",
+			ctime:   time.Now(),
+			expired: 0,
+		}
+		return uint64(0), nil
+	}
+
+	val, _ := strconv.Atoi(v.val)
+	val--
+	if val < 0 {
+		val = 0
+	}
+	self.items[key] = &MemoryCacheItem{
+		val:     strconv.Itoa(val),
+		ctime:   time.Now(),
+		expired: 0,
+	}
+
+	return uint64(val), nil
+}
+
+func (self *MemoryCache) Exist(key string) bool {
+	self.RLock()
+	defer self.RUnlock()
+	if v, ok := self.items[key]; ok {
+		return !v.isExpired()
+	}
+	return false
+}
+
+func (self *MemoryCache) GetMulti(keys []string) (map[string]string, error) {
+	vals := make(map[string]string)
+	for _, key := range keys {
+		val, err := self.Get(key)
+		if err != nil {
+			return nil, err
+		}
+		vals[key] = val
+	}
+	return vals, nil
+}
+
+func (self *MemoryCache) ClearAll() error {
+	self.Lock()
+	defer self.Unlock()
+	self.items = make(map[string]*MemoryCacheItem)
+	return nil
+}
